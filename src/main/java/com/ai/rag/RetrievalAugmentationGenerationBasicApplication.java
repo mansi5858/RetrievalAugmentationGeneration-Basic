7b62680b1de7da5c69c5c8b3ab38ef0728179788
Mansi Gupta <mansigupta5858@gmail.com>
2025-12-28 16:58:58 -0800

    First commit

---
+++ src/main/java/com/ai/rag/RetrievalAugmentationGenerationBasicApplication.java
@@ -0,0 +1,13 @@
+package com.ai.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrievalAugmentationGenerationBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrievalAugmentationGenerationBasicApplication.class, args);
+	}
+
+}
